619e7451d761b1ae87dbad044f157da9320b46c2
Hyjpark <hyej042@gmail.com>
2025-08-08 11:38:59 +0900

    first commit

---
+++ src/main/java/org/example/scheduleapiv2/ScheduleApiV2Application.java
@@ -0,0 +1,13 @@
+package org.example.scheduleapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleApiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApiV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/scheduleapiv2/ScheduleApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scheduleapiv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApiV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
