969b0cef5a403e5f956ab5a0dd23ba90af277491
Hyjpark <hyej042@gmail.com>
2025-08-11 18:31:08 +0900

    feat: 유저 입력 검증 기능 추가 및 예외 처리
    - build.gradle에 spring-boot-starter-validation 의존성 추가
    - 글로벌 예외 처리기에서 MethodArgumentNotValidException 처리 로직 구현하여 유효성 검사 실패 시 필드별 오류 응답
    - UserController의 사용자 생성 및 수정 API 요청 DTO에 @Valid 애노테이션 적용으로 입력 검증 추가

--- src/main/java/org/example/scheduleapiv2/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/scheduleapiv2/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,36 @@
 package org.example.scheduleapiv2.common.exception;
 
 import org.example.scheduleapiv2.common.error.ErrorCode;
+import org.example.scheduleapiv2.common.error.GlobalErrorCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorCode errorCode = GlobalErrorCode.INVALID_PARAMETER;
+
+        List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(ErrorResponse.ValidationError::of)
+                .collect(Collectors.toList());
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .errors(validationErrorList)
+                .build();
+
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(errorResponse);
+    }
 
     private ResponseEntity<ErrorResponse> handleExceptionInternal(ErrorCode errorCode) {
         return ResponseEntity.status(errorCode.getHttpStatus()).body(makeErrorResponse(errorCode));
--- src/main/java/org/example/scheduleapiv2/user/dto/UserCreateRequest.java
+++ src/main/java/org/example/scheduleapiv2/user/dto/UserCreateRequest.java
@@ -1,11 +1,24 @@
 package org.example.scheduleapiv2.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserCreateRequest {
 
+    @NotBlank(message = "이름을 입력해주세요.")
+    @Size(min = 2, max = 18, message = "이름은 2자 이상 18자 이하로 입력해주세요.")
     private String name;
+
+    @NotBlank(message = "이메일 주소를 입력해주세요.")
+    @Email
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Size(min = 4, max = 20, message = "비밀번호는 4자 이상 20자 이하로 입력해주세요")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "영어 알파벳과 숫자만 입력할 수 있습니다.")
     private String password;
 }
