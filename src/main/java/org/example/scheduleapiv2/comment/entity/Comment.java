49a6bd7c797fa9c12d8c5508ad8b5b73642765d8
Hyjpark <hyej042@gmail.com>
2025-08-12 14:28:46 +0900

    feat: 댓글(Comment) 엔티티 생성 및 일정, 유저와 연관관계 매핑

---
+++ src/main/java/org/example/scheduleapiv2/comment/entity/Comment.java
@@ -0,0 +1,26 @@
+package org.example.scheduleapiv2.comment.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.scheduleapiv2.common.entity.BaseEntity;
+import org.example.scheduleapiv2.schedule.entity.Schedule;
+import org.example.scheduleapiv2.user.entity.User;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String contents;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+}
