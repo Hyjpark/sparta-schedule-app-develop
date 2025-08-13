c023c97080f51d3010d47712cdf3aa40e3f29a51
Hyjpark <hyej042@gmail.com>
2025-08-13 17:36:27 +0900

    refactor(user): UserRepository default 메서드에서 USER_NOT_FOUND 예외 사용

--- src/main/java/org/example/scheduleapiv2/user/error/UserErrorCode.java
+++ src/main/java/org/example/scheduleapiv2/user/error/UserErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
     EMAIL_DUPLICATION(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
-    PASSWORD_MISMATCH(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+    PASSWORD_MISMATCH(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
